6250b415af438ec4ab221ab051f04a8a26f974fc
Elliot Harris <elliot.malichai.harris@gmail.com>
2025-09-09 12:55:31 +1000

    Add map editor and live view to local GUI

--- src/altinet/altinet/localnode/urls.py
+++ src/altinet/altinet/localnode/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("map/", views.map_builder, name="map_builder"),
+    path("live/", views.live_view, name="live_view"),
 ]
--- src/altinet/localnode/views.py
+++
@@ -1,8 +0,0 @@
-"""Views for the Altinet local node GUI."""
-
-from django.shortcuts import render
-
-
-def home(request):
-    """Render the landing page for the local node."""
-    return render(request, "index.html")
